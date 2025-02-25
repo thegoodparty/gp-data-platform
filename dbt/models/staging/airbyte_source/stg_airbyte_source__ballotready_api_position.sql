5f3e7d2a9f8a171f703f986d6b196a456f735c1d
Hugh Karimi <hugh@goodparty.org>
2025-02-25 21:00:00 +0000

    update dbr project name

--- dbt/models/staging/airbyte_source/stg_airbyte_source__ballotready_api_position.sql
+++ dbt/models/staging/airbyte_source/stg_airbyte_source__ballotready_api_position.sql
@@ -1,66 +1,67 @@
-with 
+with
 
-source as (
+    source as (
 
-    select * from {{ source('airbyte_source', 'ballotready_api_position') }}
+        select * from {{ source("airbyte_source", "ballotready_api_position") }}
 
-),
+    ),
 
-renamed as (
+    renamed as (
 
-    select
-        _airbyte_raw_id,
-        _airbyte_extracted_at,
-        _airbyte_meta,
-        _airbyte_generation_id,
-        id,
-        name,
-        slug,
-        tier,
-        geoid,
-        level,
-        mtfcc,
-        seats,
-        state,
-        issues,
-        salary,
-        judicial,
-        roworder,
-        appointed,
-        createdat,
-        retention,
-        updatedat,
-        databaseid,
-        hasprimary,
-        minimumage,
-        description,
-        filingphone,
-        subareaname,
-        partisantype,
-        subareavalue,
-        filingaddress,
-        staggeredterm,
-        employmenttype,
-        mustberesident,
-        maximumfilingfee,
-        runningmatestyle,
-        selectionsallowed,
-        filingrequirements,
-        normalizedposition,
-        electionfrequencies,
-        hasunknownboundaries,
-        mustberegisteredvoter,
-        paperworkinstructions,
-        hasmajorityvoteprimary,
-        hasrankedchoicegeneral,
-        hasrankedchoiceprimary,
-        eligibilityrequirements,
-        rankedchoicemaxvotesgeneral,
-        rankedchoicemaxvotesprimary,
-        musthaveprofessionalexperience
+        select
+            _airbyte_raw_id,
+            _airbyte_extracted_at,
+            _airbyte_meta,
+            _airbyte_generation_id,
+            id,
+            name,
+            slug,
+            tier,
+            geoid,
+            level,
+            mtfcc,
+            seats,
+            state,
+            issues,
+            salary,
+            judicial,
+            roworder,
+            appointed,
+            createdat,
+            retention,
+            updatedat,
+            databaseid,
+            hasprimary,
+            minimumage,
+            description,
+            filingphone,
+            subareaname,
+            partisantype,
+            subareavalue,
+            filingaddress,
+            staggeredterm,
+            employmenttype,
+            mustberesident,
+            maximumfilingfee,
+            runningmatestyle,
+            selectionsallowed,
+            filingrequirements,
+            normalizedposition,
+            electionfrequencies,
+            hasunknownboundaries,
+            mustberegisteredvoter,
+            paperworkinstructions,
+            hasmajorityvoteprimary,
+            hasrankedchoicegeneral,
+            hasrankedchoiceprimary,
+            eligibilityrequirements,
+            rankedchoicemaxvotesgeneral,
+            rankedchoicemaxvotesprimary,
+            musthaveprofessionalexperience
 
-    from source
+        from source
 
-)
+    )
 
-select * from renamed
+select *
+from renamed
