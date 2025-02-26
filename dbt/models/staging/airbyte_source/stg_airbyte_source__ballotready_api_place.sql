c484108d402de3b329328b50825ae090f84f40f0
Hugh Karimi <hussain.h.karimi@gmail.com>
2025-02-26 17:19:56 -0500

    [WEB-3464] initialize dbt with ballotready data (#4)

---
+++ dbt/models/staging/airbyte_source/stg_airbyte_source__ballotready_api_place.sql
@@ -0,0 +1,39 @@
+with
+
+    source as (select * from {{ source("airbyte_source", "ballotready_api_place") }}),
+
+    renamed as (
+
+        select
+            _airbyte_raw_id,
+            _airbyte_extracted_at,
+            _airbyte_meta,
+            _airbyte_generation_id,
+            id,
+            name,
+            slug,
+            urls,
+            forms,
+            geoid,
+            mtfcc,
+            state,
+            contacts,
+            timezone,
+            addresses,
+            createdat,
+            dissolved,
+            updatedat,
+            databaseid,
+            primarytype,
+            hasvotebymail,
+            isprintingenabled,
+            registrationoptions,
+            isreceiverofvotebymailrequests,
+            canvoteinprimarywhen18bygeneral
+
+        from source
+
+    )
+
+select *
+from renamed
