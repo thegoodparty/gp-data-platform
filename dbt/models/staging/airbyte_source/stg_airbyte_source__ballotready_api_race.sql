c988f2402b5f7658b4287e303bfcd4217599b4af
Hugh Karimi <hussain.h.karimi@gmail.com>
2025-03-03 08:46:33 -0500

    fornmatting

--- dbt/models/staging/airbyte_source/stg_airbyte_source__ballotready_api_race.sql
+++ dbt/models/staging/airbyte_source/stg_airbyte_source__ballotready_api_race.sql
@@ -1,36 +1,33 @@
-with 
+with
 
-source as (
+    source as (select * from {{ source("airbyte_source", "ballotready_api_race") }}),
 
-    select * from {{ source('airbyte_source', 'ballotready_api_race') }}
+    renamed as (
 
-),
+        select
+            _airbyte_raw_id,
+            _airbyte_extracted_at,
+            _airbyte_meta,
+            _airbyte_generation_id,
+            id,
+            seats,
+            election,
+            isrecall,
+            isrunoff,
+            position,
+            createdat,
+            isprimary,
+            updatedat,
+            databaseid,
+            isdisabled,
+            ispartisan,
+            candidacies,
+            isunexpired,
+            filingperiods
 
-renamed as (
+        from source
 
-    select
-        _airbyte_raw_id,
-        _airbyte_extracted_at,
-        _airbyte_meta,
-        _airbyte_generation_id,
-        id,
-        seats,
-        election,
-        isrecall,
-        isrunoff,
-        position,
-        createdat,
-        isprimary,
-        updatedat,
-        databaseid,
-        isdisabled,
-        ispartisan,
-        candidacies,
-        isunexpired,
-        filingperiods
+    )
 
-    from source
-
-)
-
-select * from renamed
+select *
+from renamed
