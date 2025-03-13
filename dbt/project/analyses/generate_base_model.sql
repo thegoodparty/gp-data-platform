155f9edb7082c51d4bdc625c7248bc7b63173623
Hugh Karimi <hussain.h.karimi@gmail.com>
2025-03-13 16:34:59 -0400

    [WEB-3856] dbt: add county and cities

--- dbt/project/analyses/generate_base_model.sql
+++ dbt/project/analyses/generate_base_model.sql
@@ -1,7 +1,31 @@
 {{
     codegen.generate_base_model(
         source_name="airbyte_source",
-        table_name="ballotready_api_mtfcc",
+        table_name="ballotready_s3_uscities_v1.77",
+        materialized="view",
+    )
+}}
+
+{{
+    codegen.generate_base_model(
+        source_name="airbyte_source",
+        table_name="ballotready_s3_uscities_v1.77_short",
+        materialized="view",
+    )
+}}
+
+{{
+    codegen.generate_base_model(
+        source_name="airbyte_source",
+        table_name="ballotready_s3_uscounties_v1.73",
+        materialized="view",
+    )
+}}
+
+{{
+    codegen.generate_base_model(
+        source_name="airbyte_source",
+        table_name="ballotready_s3_uscounties_v1.73_short",
         materialized="view",
     )
 }}
