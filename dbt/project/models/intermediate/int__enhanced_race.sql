814f8c750163944a0d3f81dbc98298339c692747
Hugh Karimi <hussain.h.karimi@gmail.com>
2025-03-19 16:05:54 -0400

    add frequency

--- dbt/project/models/intermediate/int__enhanced_race.sql
+++ dbt/project/models/intermediate/int__enhanced_race.sql
@@ -5,33 +5,40 @@ schema to build:
 model Race {
   electionDate            DateTime      @map("election_date")
   electionDay             String        @map("election_day")
-  state                   String?       @db.Char(2)
   // Position
   locationName            String?       @map("location_name")
   filingDateStart         String?       @map("filing_date_start")
   filingDateEnd           String?       @map("filing_date_end")
-  salary                  String?       @map("salary")
   // PositionElection
-  frequency               Int[]         @map("frequency")
-  county                  County?       @relation(fields: [countyId], references: [id])
-  # look at Place, geoId, etc
-  municipality            Municipality? @relation(fields: [municipalityId], references: [id])
-  candidacies             Candidacy[]
-  countyId                String?       @db.Uuid
-  municipalityId          String?       @db.Uuid
+  Place                   Place?        @relation(fields: [placeId], references: [id])
+  placeId                 String?       @db.Uuid
 }
 */
 with
-    enhnced_race as (
+    election_frequencies as (
+        select tbl_pos.database_id, e.databaseid as pe_frequency_database_id
+        from {{ ref("stg_airbyte_source__ballotready_api_position") }} as tbl_pos
+        lateral view explode(tbl_pos.election_frequencies) exploded_table as e
+    ),
+    election_frequency as (
+        select tbl_pos.database_id as position_database_id, tbl_freq.frequency
+        from election_frequencies as tbl_pos
+        left join
+            {{ ref("int__ballotready_position_election_frequency") }} as tbl_freq
+            on tbl_pos.pe_frequency_database_id = tbl_freq.database_id
+        where tbl_freq.valid_on is null
+    ),
+    enhanced_race as (
         select
             uuid(md5(concat("ballotready", "-", tbl_race.id))) as id,
             tbl_race.id as br_hash_id,
             tbl_race.database_id as br_database_id,
             tbl_race.is_primary,
             tbl_race.is_runoff,
-            tbl_election.election_day as election_date,
+            tbl_race.created_at,
+            tbl_race.updated_at,
+            to_timestamp(tbl_election.election_day) as election_date,
             tbl_position.slug as position_slug,
-            concat(tbl_position.state, "-", tbl_position.slug) as state_slug,
             tbl_position.state as `state`,
             tbl_position.level as position_level,
             tbl_normalized_position.name as normalized_position_name,
@@ -40,12 +47,14 @@ with
             tbl_position.filing_address as filing_office_address,
             tbl_position.filing_phone as filing_phone_number,
             tbl_position.paperwork_instructions,
-            tbl_position.filing_requirementsm,
+            tbl_position.filing_requirements,
             tbl_position.partisan_type,
             tbl_position.employment_type,
             tbl_position.eligibility_requirements,
-            tbl_position.salary
-
+            tbl_position.salary,
+            tbl_position.sub_area_name,
+            tbl_position.sub_area_value,
+            tbl_election_frequency.frequency
         from {{ ref("stg_airbyte_source__ballotready_api_race") }} as tbl_race
         left join
             {{ ref("stg_airbyte_source__ballotready_api_election") }} as tbl_election
@@ -57,6 +66,13 @@ with
             {{ ref("int__ballotready_normalized_position") }} as tbl_normalized_position
             on tbl_position.normalized_position.`databaseId`
             = tbl_normalized_position.database_id
+        left join
+            {{ ref("int__ballotready_position_election_frequency") }} as tbl_frequency
+            on tbl_position.election_frequencies[0].databaseid
+            = tbl_frequency.database_id
+        left join
+            election_frequency as tbl_election_frequency
+            on tbl_position.database_id = tbl_election_frequency.position_database_id
     )
 
 select *
