465f13f7c03af28318d6d34898883dbf2e4f00f1
Hugh Karimi <hussain.h.karimi@gmail.com>
2025-03-21 19:37:01 -0400

    add candidacy mart

--- dbt/project/models/mart/election_api/m_election_api__candidacy.sql
+++ dbt/project/models/mart/election_api/m_election_api__candidacy.sql
@@ -7,24 +7,32 @@
     )
 }}
 
-/*
+with
+    candidacy as (
+        select
+            {{ generate_salted_uuid(fields=["id"], salt="ballotready") }} as id,
+            id as br_hash_id,
+            database_id as br_database_id,
+            created_at,
+            updated_at,
+            is_certified,
+            is_hidden,
+            withdrawn,
+            result
+        from {{ ref("int__ballotready_candidacy") }}
+        {% if is_incremental() %}
+            where updated_at > (select max(updated_at) from {{ this }})
+        {% endif %}
+    )
+
 select
-    {{ generate_salted_uuid(fields=["id"], salt="ballotready") }}
-    as id,
-    id as br_hash_id,
-    database_id as br_database_id,
+    id,
+    br_hash_id,
+    br_database_id,
     created_at,
     updated_at,
     is_certified,
     is_hidden,
     withdrawn,
     result
-from {{ ref("int__ballotready_candidacy") }}
-{% if is_incremental() %}
-    where updated_at > (select max(updated_at) from {{ this }})
-{% endif %}
-
-*/
-select 1 as dummy_for_eof_formatting
-from (select 1)
-;
+from candidacy
