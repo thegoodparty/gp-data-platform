a3d3dbf2ec440ec9dad35d452565b714cf9d91e2
Hugh Karimi <hussain.h.karimi@gmail.com>
2025-03-24 12:56:54 -0400

    process cities stageing

--- dbt/project/models/intermediate/int__place_fun_facts.sql
+++ dbt/project/models/intermediate/int__place_fun_facts.sql
@@ -2,7 +2,7 @@
     config(
         materialized="incremental",
         incremental_strategy="merge",
-        unique_key="id",
+        unique_key="database_id",
         tags=["intermediate", "ballotready", "place_fun_facts"],
     )
 }}
@@ -10,8 +10,8 @@
 with
     joined_by_geo_id as (
         select
-            cast(tbl_place.databaseid as int) as database_id,
-            tbl_place.geoid,
+            cast(tbl_place.database_id as int) as database_id,
+            tbl_place.geo_id,
             tbl_place.name,
             tbl_place.slug,
             tbl_place.state,
@@ -19,12 +19,22 @@ with
             tbl_cities.county_fips as county_fips,
             tbl_cities.city,
             tbl_cities.zips,
-            tbl_cities.csa_name
+            tbl_cities.csa_name,
+            tbl_cities.population,
+            tbl_cities.density,
+            tbl_cities.home_value,
+            tbl_cities.unemployment_rate,
+            tbl_cities.income_household_median
         from {{ ref("stg_airbyte_source__ballotready_api_place") }} as tbl_place
         left join
             {{ ref("stg_airbyte_source__ballotready_s3_uscities_v1_77") }} as tbl_cities
-            on substring(tbl_place.geoid, 1, 5) = tbl_cities.county_fips
-    ),
+            on substring(tbl_place.geo_id, 1, 5) = tbl_cities.county_fips
+            and tbl_place.state = tbl_cities.state_id
+            and tbl_cities.population is not null
+        {% if is_incremental() %}
+            where tbl_place.updated_at > (select max(updated_at) from {{ this }})
+        {% endif %}
+    )
 
 /*
 with
