55292a14e6fda71a0dc1a0424145cd71864cad42
Hugh Karimi <hussain.h.karimi@gmail.com>
2025-04-01 17:09:05 -0400

    add data types

--- dbt/project/models/staging/airbyte_source/stg_airbyte_source__hubspot_api_owners.sql
+++ dbt/project/models/staging/airbyte_source/stg_airbyte_source__hubspot_api_owners.sql
@@ -6,16 +6,18 @@ with
             {{ adapter.quote("_airbyte_extracted_at") }},
             {{ adapter.quote("_airbyte_meta") }},
             {{ adapter.quote("_airbyte_generation_id") }},
-            {{ adapter.quote("id") }},
+            int({{ adapter.quote("id") }}) as id,
             {{ adapter.quote("email") }},
             {{ adapter.quote("teams") }},
             {{ adapter.quote("userId") }} as user_id,
             {{ adapter.quote("archived") }},
             {{ adapter.quote("lastName") }} as last_name,
-            {{ adapter.quote("createdAt") }} as created_at,
+            to_timestamp({{ adapter.quote("createdAt") }}) as created_at,
             {{ adapter.quote("firstName") }} as first_name,
-            {{ adapter.quote("updatedAt") }} as updated_at,
-            {{ adapter.quote("userIdIncludingInactive") }} as user_id_including_inactive
+            to_timestamp({{ adapter.quote("updatedAt") }}) as updated_at,
+            int(
+                {{ adapter.quote("userIdIncludingInactive") }}
+            ) as user_id_including_inactive
 
         from source
     )
