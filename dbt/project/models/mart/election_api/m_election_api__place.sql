2a01fd7d976d259bf761603a0faf1d17e76ee58e
Hugh Karimi <hussain.h.karimi@gmail.com>
2025-04-11 17:38:32 -0400

    fix place name in place mart

--- dbt/project/models/mart/election_api/m_election_api__place.sql
+++ dbt/project/models/mart/election_api/m_election_api__place.sql
@@ -8,25 +8,34 @@
 }}
 
 
+with
+    deduped_place as (
+        select *
+        from {{ ref("int__enhanced_place") }}
+        qualify row_number() over (partition by geo_id order by population desc) = 1
+    )
 select
-    id,
-    created_at,
-    updated_at,
-    br_database_id as br_position_database_id,
-    `name`,
-    slug,
-    geo_id as geoid,
-    mtfcc,
-    `state`,
-    city_largest,
-    county_name,
-    population,
-    density,
-    income_household_median,
-    unemployment_rate,
-    home_value,
-    parent_id
-from {{ ref("int__enhanced_position_w_parent") }}  -- note that the position table is used for the election place table
-{% if is_incremental() %}
-    where updated_at > (select max(updated_at) from {{ this }})
-{% endif %}
+    tbl_pos.id,
+    tbl_pos.created_at,
+    tbl_pos.updated_at,
+    tbl_pos.br_database_id as br_position_database_id,
+    tbl_place.`name`,
+    tbl_pos.slug,
+    tbl_pos.geo_id as geoid,
+    tbl_pos.mtfcc,
+    tbl_pos.`state`,
+    tbl_pos.city_largest,
+    tbl_pos.county_name,
+    tbl_pos.population,
+    tbl_pos.density,
+    tbl_pos.income_household_median,
+    tbl_pos.unemployment_rate,
+    tbl_pos.home_value,
+    tbl_pos.parent_id
+from {{ ref("int__enhanced_position_w_parent") }} as tbl_pos  -- note that the position table is used for the election place table
+left join deduped_place as tbl_place on tbl_pos.geo_id = tbl_place.geo_id
+where
+    tbl_pos.geo_id is not null and tbl_pos.slug is not null
+    {% if is_incremental() %}
+        and tbl_pos.updated_at > (select max(updated_at) from {{ this }})
+    {% endif %}
