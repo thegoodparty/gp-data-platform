563d781227fbdcb8a5867837ca5bb171ce4101b7
Hugh Karimi <hussain.h.karimi@gmail.com>
2025-04-14 16:23:17 -0400

    update place mart

--- dbt/project/models/mart/election_api/m_election_api__place.sql
+++ dbt/project/models/mart/election_api/m_election_api__place.sql
@@ -8,36 +8,30 @@
 }}
 
 
-with
-    deduped_place as (
-        select *
-        from {{ ref("int__enhanced_place") }}
-        qualify row_number() over (partition by geo_id order by population desc) = 1
-    )
 select
-    tbl_pos.id,
-    tbl_pos.created_at,
-    tbl_pos.updated_at,
-    tbl_pos.br_database_id as br_position_database_id,
+    tbl_place.id,
+    tbl_place.created_at,
+    tbl_place.updated_at,
+    tbl_place.br_database_id,
     tbl_place.`name`,
-    tbl_pos.slug,
-    tbl_pos.geo_id as geoid,
-    tbl_pos.mtfcc,
-    tbl_pos.`state`,
-    tbl_pos.city_largest,
-    tbl_pos.county_name,
-    tbl_pos.population,
-    tbl_pos.density,
-    tbl_pos.income_household_median,
-    tbl_pos.unemployment_rate,
-    tbl_pos.home_value,
-    tbl_pos.parent_id
-from {{ ref("int__enhanced_position_w_parent") }} as tbl_pos  -- note that the position table is used for the election place table
-left join deduped_place as tbl_place on tbl_pos.geo_id = tbl_place.geo_id
+    tbl_place.place_name_slug as slug,
+    tbl_place.geo_id as geoid,
+    tbl_place.mtfcc,
+    tbl_place.`state`,
+    tbl_place.city_largest,
+    tbl_place.county_name,
+    tbl_place.population,
+    tbl_place.density,
+    tbl_place.income_household_median,
+    tbl_place.unemployment_rate,
+    tbl_place.home_value,
+    tbl_place.parent_geo_id,
+    tbl_place.parent_id
+from {{ ref("int__enhanced_place_w_parent") }} as tbl_place
 where
-    tbl_pos.geo_id is not null
-    and tbl_pos.slug is not null
+    tbl_place.geo_id is not null
+    and tbl_place.place_name_slug is not null
     and tbl_place.name is not null
     {% if is_incremental() %}
-        and tbl_pos.updated_at > (select max(updated_at) from {{ this }})
+        and tbl_place.updated_at > (select max(updated_at) from {{ this }})
     {% endif %}
