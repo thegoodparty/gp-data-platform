aaf52581be2af9ec3dd56a398116c1fa07d1fc79
Hugh Karimi <hussain.h.karimi@gmail.com>
2025-04-25 10:58:01 -0400

    add people/candidate rows to candidacy

--- dbt/project/models/intermediate/int__enhanced_candidacy.sql
+++ dbt/project/models/intermediate/int__enhanced_candidacy.sql
@@ -3,36 +3,60 @@
         materialized="incremental",
         incremental_strategy="merge",
         unique_key="id",
-        tags=["intermediate", "candidacy"],
+        tags=["intermediate", "ballotready", "enhanced_candidacy"],
     )
 }}
 
 with
-    candidacy as (
+    latest_candidacy as (
         select
             {{ generate_salted_uuid(fields=["id"], salt="ballotready") }} as id,
             id as br_hash_id,
             database_id as br_database_id,
+            candidate_database_id,
             created_at,
             updated_at,
-            is_certified,
-            is_hidden,
-            withdrawn,
             result
         from {{ ref("int__ballotready_candidacy") }}
         {% if is_incremental() %}
             where updated_at > (select max(updated_at) from {{ this }})
         {% endif %}
+    ),
+    enhanced_columns as (
+        select
+            tbl_candidacy.id,
+            tbl_candidacy.br_database_id,
+            tbl_candidacy.created_at,
+            tbl_candidacy.updated_at,
+            tbl_person.first_name,
+            tbl_person.last_name,
+            -- TODO: get party,
+            -- TODO: get city and state (candidace -> race -> place)
+            case
+                when size(tbl_person.images) > 0 then tbl_person.images[0].url else null
+            end as image,
+            case
+                when tbl_person.bio_text is not null then tbl_person.bio_text else null
+            -- TODO: in case bio_text is null, use
+            -- https://github.com/thegoodparty/tgp-api/blob/74b4b6247b75cc39077ad4b16bfbe83dd997b6cf/api/helpers/candidate/generate-presentation.js#L175
+            end as about,
+            transform(tbl_person.urls, url -> url.url) as urls
+        -- TODO: join to gp-api data
+        -- TODO: add position/place data (candidacy -> race -> position)
+        from latest_candidacy as tbl_candidacy
+        left join
+            {{ ref("int__ballotready_person") }} as tbl_person
+            on tbl_candidacy.candidate_database_id = tbl_person.database_id
     )
--- TODO: add person data from int__ballotready_person
+
 select
     id,
-    br_hash_id,
     br_database_id,
     created_at,
     updated_at,
-    is_certified,
-    is_hidden,
-    withdrawn,
-    result
-from candidacy
+    first_name,
+    last_name,
+    image,
+    about,
+    urls
+from enhanced_columns
