0861a4cffefcf00e131991e48f4b16968ecce9c5
Hugh Karimi <hussain.h.karimi@gmail.com>
2025-05-05 17:50:14 -0400

    sftp file reader

--- dbt/project/models/load/load__l2_sftp_to_s3.py
+++ dbt/project/models/load/load__l2_sftp_to_s3.py
@@ -1,41 +1,53 @@
-import os
+# import os
 import re
 
 # import tempfile
-from datetime import datetime
-from typing import Callable, Dict
+# from datetime import datetime
+from typing import Callable
 
 # import pandas as pd
 from paramiko import SFTPClient, Transport
 from pyspark.sql import DataFrame
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, udf, upper
 from pyspark.sql.types import StringType
+
+# import tempfile
+# import boto3
+
 
 # from zipfile import ZipFile
 
 
-def _extract_and_load_w_sftp(sftp_client: SFTPClient) -> Callable:
+def _extract_and_load_w_sftp(
+    host: str, port: int, user: str, password: str
+) -> Callable:
     """
-    Creates a pandas UDF that extracts and loads files from an SFTP server to an S3 bucket.
+    Creates a pandas UDF that extracts and loads files from an SFTP server to an S3 bucket. Note that
+    the SFTPClient cannot be serilized to be passed to the UDF workers. The connection must be established
+    on the worker nodes.
 
     Args:
-        sftp_connection: An SFTP client connection
+        host: The host of the SFTP server
+        port: The port of the SFTP server
+        user: The user of the SFTP server
+        password: The password of the SFTP server
 
     Returns:
         A pandas UDF function
     """
 
-    def _extract_and_load(state_id: str) -> Dict[str, str | datetime | int]:
+    def _extract_and_load(state_id: str) -> str:
+        # def _extract_and_load(state_id: str) -> Dict[str, str | datetime | int]:
         # @pandas_udf(returnType=StringType())
         # def _extract_and_load(state_id: pd.Series) -> pd.Series:
         """
-        Pandas UDF to process state IDs in batches and extract and load files from SFTP to S3.
+        UDF to process state IDs and extract and load files from SFTP to S3.
 
         Args:
             state_id: Series of state IDs
 
         Returns:
-            Series of load details
+            JSON string with load details
         """
         """
         TODO: complete writing this function
@@ -49,24 +61,84 @@ def _extract_and_load_w_sftp(sftp_client: SFTPClient) -> Callable:
             it would also have a column for the state id, and time of loading and anything
             else that airbyte logs when using the sftp bulk loader
         """
-        # file_name_pattern = f"VM2--{state_id}*.zip"
-        file_path = "/VMFiles/"
-        file_list = sftp_client.listdir(file_path)
-        file_name_pattern = re.compile(r"VM2--[A-Z]{2}--\d{4}-\d{2}-\d{2}\.zip")
-        file_list = [f for f in file_list if re.match(file_name_pattern, f)]
-        if len(file_list) != 1:
-            raise ValueError(
-                f"Expected 1 file for state {state_id}, got {len(file_list)}"
-            )
+        # Create SFTP connection inside the UDF
+        transport: Transport = Transport((host, port))
+        transport.connect(username=user, password=password)
+        sftp_client: SFTPClient | None = SFTPClient.from_transport(transport)
+        if sftp_client is None:
+            raise ValueError("Failed to create SFTP client")
 
+        try:
+            # file_name_pattern = f"VM2--{state_id}*.zip"
+            file_path = "/VMFiles/"
+            file_list = sftp_client.listdir(file_path)
+            string_pattern = f"VM2--{state_id}" + r"--\d{4}-\d{2}-\d{2}\.zip"
+            file_name_pattern = re.compile(string_pattern)
+            file_list = [f for f in file_list if re.match(file_name_pattern, f)]
+            if len(file_list) != 1:
+                raise ValueError(
+                    f"Expected 1 file for state {state_id}, got {len(file_list)}\n"
+                    f"string_pattern: {string_pattern}\n"
+                    f"file_name_pattern: {file_name_pattern}\n"
+                    f"file_list: {file_list}\n"
+                )
+
+        finally:
+            # Close SFTP connection
+            sftp_client.close()
+            transport.close()
+
+        # download the file from the sftp server and extract it
         file_name = file_list[0]
-        file_path = os.path.join(file_path, file_name)
-        # with sftp_client.open(file_path, "rb") as f:
-        #     zip_file = ZipFile(f)
-        #     zip_file.extractall()
-        # return load_details
-        return {}
+        # full_file_path = os.path.join(file_path, file_name)
 
+        # Create a temporary directory to store the zip file and extracted contents
+        # with tempfile.TemporaryDirectory() as temp_dir:
+        #     local_zip_path = os.path.join(temp_dir, file_name)
+
+        #     # Download the file from the SFTP server
+        #     sftp_client.get(full_file_path, local_zip_path)
+
+        #     # Extract the zip file
+        #     file_names = []
+        #     with ZipFile(local_zip_path, 'r') as zip_file:
+        #         file_names = zip_file.namelist()
+        #         zip_file.extractall(path=temp_dir)
+
+        # # Upload extracted files to S3
+        # s3_client = boto3.client('s3')
+        # s3_prefix = f"states/{state_id}/data/"
+
+        # # Upload each extracted file to S3
+        # for extracted_file in file_names:
+        #     local_file_path = os.path.join(temp_dir, extracted_file)
+        #     s3_key = f"{s3_prefix}{extracted_file}"
+
+        #     if os.path.isfile(local_file_path):
+        #         s3_client.upload_file(
+        #             Filename=local_file_path,
+        #             Bucket=s3_bucket_name,
+        #             Key=s3_key
+        #         )
+
+        # load_time = datetime.now().isoformat()
+        # result = {
+        #     "state_id": state_id,
+        #     "file_names": file_names,
+        #     "zip_file": file_name,
+        #     "load_time": load_time,
+        #     # "s3_prefix": s3_prefix
+        # }
+
+        from json import dumps
+
+        # return dumps(result)
+
+        return dumps({"state_id": state_id, "file_names": [file_name]})
+
+    # return_type = MapType(StringType(), ArrayType(StringType()))
+    # _extract_and_load = udf(_extract_and_load, returnType=return_type)
+    _extract_and_load = udf(_extract_and_load, returnType=StringType())
     return _extract_and_load
 
 
@@ -81,48 +153,29 @@ def model(dbt, session):
         tags=["l2", "sftp", "s3", "load"],
     )
 
+    # S3 configuration
+    # s3_bucket = dbt.var("s3_bucket", "data-platform-prod")
+
     # connecting to sftp server
     sftp_host = dbt.config.get("l2_sftp_host")
-    sftp_port = dbt.config.get("l2_sftp_port")
+    sftp_port = int(dbt.config.get("l2_sftp_port"))
     sftp_user = dbt.config.get("l2_sftp_user")
     sftp_password = dbt.config.get("l2_sftp_password")
-
-    # Create SFTP connection
-    transport: Transport = Transport((sftp_host, int(sftp_port)))
-    transport.connect(username=sftp_user, password=sftp_password)
-    sftp_client: SFTPClient = SFTPClient.from_transport(transport)
 
     # get list of states
     states: DataFrame = (
         dbt.ref("stg_airbyte_source__ballotready_s3_uscities_v1_77")
         .select("state_id")
         .distinct()
-        .collect()
     )
-    states = states.withColumn("state_id", col("state_id").cast(StringType()).upper())
+    states = states.withColumn("state_id", upper(col("state_id").cast(StringType())))
 
-    extract_and_load = _extract_and_load_w_sftp(sftp_client)
-    states = states.withColumn("load_details", extract_and_load("state_id"))
+    # for dev just take the first state
+    states = states.limit(1)
 
-    # # Create a temporary directory to store downloaded files
-    # with tempfile.TemporaryDirectory() as temp_dir:
-    #     # Download files from SFTP to temporary directory
-    #     downloaded_files = []
-    #     for file_name in file_list:
-    #         if file_name.endswith('.csv'):  # Filter for CSV files or adjust as needed
-    #             remote_file_path = os.path.join(remote_path, file_name)
-    #             local_file_path = os.path.join(temp_dir, file_name)
-    #             sftp.get(remote_file_path, local_file_path)
-    #             downloaded_files.append(local_file_path)
+    extract_and_load = _extract_and_load_w_sftp(
+        sftp_host, sftp_port, sftp_user, sftp_password
+    )
+    states = states.withColumn("load_details", extract_and_load(states["state_id"]))
 
-    #     # Create DataFrame from downloaded files
-    #     if downloaded_files:
-    #         df = session.read.csv(downloaded_files, header=True, inferSchema=True)
-    #     else:
-    #         # Create empty DataFrame if no files were downloaded
-    #         empty_schema = StructType([StructField("id", StringType(), True)])
-    #         df = session.createDataFrame([], empty_schema)
-
-    # # Close SFTP connection
-    # sftp.close()
-    # transport.close()
+    return states
