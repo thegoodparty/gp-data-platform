cea24e6e98d363d8b87caa53a7f354c19cea67a2
Hugh Karimi <hussain.h.karimi@gmail.com>
2025-05-22 18:57:17 -0400

    complete state list

---
+++ dbt/project/models/staging/dbt_source/l2_s3/stg_dbt_source__l2_s3_wy_demographic.sql
@@ -0,0 +1,4 @@
+{% set source_ref = source("dbt_source", "l2_s3_wy_demographic") %}
+
+select {{ dbt_utils.star(from=source_ref, except=[]) }}  -- use `except` for any columns to transform individually
+from {{ source_ref }}
