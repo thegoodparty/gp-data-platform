0651b636dbcd05738aa4d8e48a306e796e5c52e7
Hugh Karimi <hussain.h.karimi@gmail.com>
2025-06-03 16:51:00 -0400

    codegen and staging data model as views

---
+++ dbt/project/models/staging/dbt_source/l2_s3/stg_dbt_source__l2_s3_al_uniform_data_dictionary.sql
@@ -0,0 +1,6 @@
+{{ config(materialized="view") }}
+
+{% set source_ref = source("dbt_source", "l2_s3_al_uniform_data_dictionary") %}
+
+select {{ dbt_utils.star(from=source_ref, except=[]) }}  -- use `except` for any columns to transform individually
+from {{ source_ref }}
