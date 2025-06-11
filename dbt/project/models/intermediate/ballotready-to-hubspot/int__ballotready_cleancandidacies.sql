bf62a7f777212ce9fa7c41911d923d3eb6792690
Tristan Aubert <tristan.aubert@goodparty.org>
2025-06-11 14:57:59 +0000

    Added files for obtaining BallotReady records populated with contact details that can be directly sent to HubSpot

---
+++ dbt/project/models/intermediate/ballotready-to-hubspot/int__ballotready_cleancandidacies.sql
@@ -0,0 +1,245 @@
+  -- First step in extracting of BallotReady data for updating to hubspot  
+  -- This file reads new ballotReady data and applies relevant field transformations
+  -- Select the difference between the current BallotReady data and the previous
+  -- BallotReady data
+with br_new as ( 
+    select * from dbt.stg_airbyte_source__ballotready_s3_candidacies_v3
+    WHERE candidacy_updated_at > current_date - INTERVAL 1 MONTH
+),
+br_formatted as (
+select
+    -- ensure that there are not duplicates *within* the new BallotReady candidacies
+    distinct
+        election_day as election_date,
+        position_name as official_office_name,
+        case
+          when position_name like '%Village President%' then 'Village President'
+          when position_name like '%Town Chair%' then 'Town Chair'
+          when position_name like '%Water Supply District%' then 'Water Supply Board'
+          when normalized_position_name = 'City Legislature' then 'City Council'
+          when normalized_position_name = 'Local School Board' then 'School Board'
+          when normalized_position_name = 'City Executive//Mayor' then 'Mayor'
+          when normalized_position_name = 'Township Treasurer//Fiscal Officer' then 'Treasurer'
+          when normalized_position_name = 'Township Trustee//Township Council' then 'Township Council'
+          when normalized_position_name = 'Parks and Recreation District Board' then 'Parks and Recreation Board'
+          when normalized_position_name = 'Local Higher Education Board//Community College Board' then 'Community College Board'
+          when normalized_position_name = 'City Clerk//Ward Officer' then 'City Clerk'
+          when normalized_position_name = 'Library District Board' then 'Library Board'
+          when normalized_position_name = 'Fire District Board' then 'Fire Board'
+          when normalized_position_name = 'Township Highway Superintendent' then 'Highway Superintendent'
+          when normalized_position_name = 'Township Constable' then 'Constable'
+          when normalized_position_name = 'City Treasurer//Finance Officer' then 'Treasurer'
+          when normalized_position_name = 'State Trial Court Judge - General' then 'Trial Court Judge'
+          when normalized_position_name = 'Township Clerk/Treasurer (Joint)' then 'Township Clerk'
+          when normalized_position_name = 'Local Court Judge' then 'Judge'
+          when normalized_position_name = 'State Senator' then 'State Senate'
+          when normalized_position_name = 'City Assessor//Lister' then 'City Assessor'
+          when normalized_position_name = 'County Legislature//Executive Board' then 'County Legislature'
+          when normalized_position_name = 'Harbor District Board//Port District Board' then 'Port Board'
+          else normalized_position_name
+        end as candidate_office,
+        case
+          when level = 'local' then 'Local'
+          when level = 'city' then 'City'
+          when level = 'county' then 'County'
+          when level = 'state' then 'State'
+          else level
+        end as office_level, 
+        tier as candidate_id_tier,
+        number_of_seats as number_of_seats_available,
+        case
+          when is_primary = 'false' then 'General'
+          when is_primary = 'true' then 'Primary'
+        end as election_type,
+        case
+          when parties like '%Independent%' then 'Independent'
+          when parties like '%Nonpartisan%' then 'Nonpartisan'
+          else ''
+        end as party_affiliation,
+        concat_ws(
+            '|',transform(from_json(regexp_replace(parties, '=>', ':'), 
+            'array<struct<name:string, short_name:string>>' ),x -> x.name)
+        ) AS party_list,
+        parties,
+        first_name as first_name,
+        middle_name as middle_name,
+        last_name as last_name,
+        case
+          when state = 'AK' then 'Alaska'
+          when state = 'AL' then 'Alabama'
+          when state = 'AR' then 'Arkansas'
+          when state = 'AZ' then 'Arizona'
+          when state = 'CA' then 'California'
+          when state = 'CO' then 'Colorado'
+          when state = 'CT' then 'Connecticut'
+          when state = 'DC' then 'District of Columbia'
+          when state = 'DE' then 'Delaware'
+          when state = 'FL' then 'Florida'
+          when state = 'GA' then 'Georgia'
+          when state = 'HI' then 'Hawaii'
+          when state = 'IA' then 'Iowa'
+          when state = 'ID' then 'Idaho'
+          when state = 'IL' then 'Illinois'
+          when state = 'IN' then 'Indiana'
+          when state = 'KS' then 'Kansas'
+          when state = 'KY' then 'Kentucky'
+          when state = 'LA' then 'Louisiana'
+          when state = 'MA' then 'Massachusetts'
+          when state = 'MD' then 'Maryland'
+          when state = 'ME' then 'Maine'
+          when state = 'MI' then 'Michigan'
+          when state = 'MN' then 'Minnesota'
+          when state = 'MO' then 'Missouri'
+          when state = 'MS' then 'Mississippi'
+          when state = 'MT' then 'Montana'
+          when state = 'NC' then 'North Carolina'
+          when state = 'ND' then 'North Dakota'
+          when state = 'NE' then 'Nebraska'
+          when state = 'NH' then 'New Hampshire'
+          when state = 'NJ' then 'New Jersey'
+          when state = 'NM' then 'New Mexico'
+          when state = 'NV' then 'Nevada'
+          when state = 'NY' then 'New York'
+          when state = 'OH' then 'Ohio'
+          when state = 'OK' then 'Oklahoma'
+          when state = 'OR' then 'Oregon'
+          when state = 'PA' then 'Pennsylvania'
+          when state = 'RI' then 'Rhode Island'
+          when state = 'SC' then 'South Carolina'
+          when state = 'SD' then 'South Dakota'
+          when state = 'TN' then 'Tennessee'
+          when state = 'TX' then 'Texas'
+          when state = 'UT' then 'Utah'
+          when state = 'VA' then 'Virginia'
+          when state = 'VT' then 'Vermont'
+          when state = 'WA' then 'Washington'
+          when state = 'WI' then 'Wisconsin'
+          when state = 'WV' then 'West Virginia'
+          when state = 'WY' then 'Wyoming'
+        else state
+        end as state,
+        replace(
+            replace(
+                replace(
+                        replace(phone, '-', ''),
+                    '_', ''
+                ),
+                '[',''
+            ),
+            ']',''
+         ) as phone,
+        email,
+        case 
+            when official_office_name like '% County%'
+                then left(official_office_name, position(' County' in official_office_name) - 1)
+            when official_office_name like '% City%'
+                then left(official_office_name, position(' City' in official_office_name) - 1)
+            when official_office_name like '% Village%'
+                then left(official_office_name, position(' Village' in official_office_name) - 1)
+            when official_office_name like '% Town%'
+                then left(official_office_name, position(' Town' in official_office_name) - 1)
+            when official_office_name like '% Fire%'
+                then left(official_office_name, position(' Fire' in official_office_name) - 1)
+            when official_office_name like '% Public Library%'
+                then left(official_office_name, position(' Public Library' in official_office_name) - 1)
+            when official_office_name like '% Library%'
+                then left(official_office_name, position(' Library' in official_office_name) - 1)
+            when official_office_name like '% Community College%'
+                then left(official_office_name, position(' Community College' in official_office_name) - 1)
+            when official_office_name like '% Water District%'
+                then left(official_office_name, position(' Water District' in official_office_name) - 1)
+            when position(' District' in official_office_name) > 0
+                and (
+                    regexp_extract(trim(official_office_name), '- District [^ ]+$', 0) = '' 
+                    or position(' District' in official_office_name) <
+                        length(official_office_name) - length(regexp_extract(trim(official_office_name), '- District [^ ]+$', 0)) + 1
+                )
+                then left(official_office_name, position(' District' in official_office_name) - 1)
+            when official_office_name like '% Independent School%'
+                then left(official_office_name, position(' Independent School' in official_office_name) - 1)
+            when official_office_name like '% Elementary School%'
+                then left(official_office_name, position(' Elementary School' in official_office_name) - 1)
+            when official_office_name like '% Area Unified School%'
+                then left(official_office_name, position(' Area Unified School' in official_office_name) - 1)
+            when official_office_name like '% Area School%'
+                then left(official_office_name, position(' Area School' in official_office_name) - 1)
+            when official_office_name like '% Area Public School%'
+                then left(official_office_name, position(' Area Public School' in official_office_name) - 1)
+            when official_office_name like '% Union School%'
+                then left(official_office_name, position(' Union School' in official_office_name) - 1)
+            when official_office_name like '% City School%'
+                then left(official_office_name, position(' City School' in official_office_name) - 1)
+            when official_office_name like '% Unified School%'
+                then left(official_office_name, position(' Unified School' in official_office_name) - 1)
+            when official_office_name like '% Public School%'
+                then left(official_office_name, position(' Public School' in official_office_name) - 1)
+            when official_office_name like '% Community School%'
+                then left(official_office_name, position(' Community School' in official_office_name) - 1)
+            when official_office_name like '% School%'
+                then left(official_office_name, position(' School' in official_office_name) - 1)
+            when official_office_name like '% Park District%'
+                then left(official_office_name, position(' Park District' in official_office_name) - 1)
+            else ''
+        end as city,
+        case 
+            when official_office_name like '%- District %' 
+                then regexp_extract(official_office_name, '- District (.*)$')
+            when official_office_name like '% - Ward %' 
+                then regexp_extract(official_office_name, ' - Ward (.*)$')
+            when official_office_name like '% - Place %' 
+                then regexp_extract(official_office_name, ' - Place (.*)$')
+            when official_office_name like '% - Branch %' 
+                then regexp_extract(official_office_name, ' - Branch (.*)$')
+            when official_office_name like '% - Subdistrict %' 
+                then regexp_extract(official_office_name, ' - Subdistrict (.*)$')
+            when official_office_name like '% - Zone %' 
+                then regexp_extract(official_office_name, ' - Zone (.*)$')
+            else ''
+        end as district,
+       CASE
+            WHEN official_office_name LIKE '% - Seat %'
+                THEN REGEXP_EXTRACT(official_office_name, ' - Seat ([^,]+)')
+            WHEN official_office_name LIKE '% - Group %'
+                THEN REGEXP_EXTRACT(official_office_name, ' - Group ([^,]+)')
+            WHEN official_office_name LIKE '%, Seat %'
+                THEN REGEXP_EXTRACT(official_office_name, ', Seat ([^,]+)')
+            WHEN official_office_name LIKE '%: % - Seat %'
+                THEN REGEXP_EXTRACT(official_office_name, ' - Seat ([^,]+)')
+            WHEN official_office_name LIKE '% - Position %'
+                THEN REGEXP_EXTRACT(official_office_name, ' - Position ([^\\s(]+)')
+            ELSE ''
+        END AS seat,
+        {{ map_office_type('candidate_office') }} AS office_type,
+        'Self-Filer Lead' as type,
+        'jesse@goodparty.org' as contact_owner,
+        'Jesse Diliberto' as owner_name,
+        'Ballotready' as candidate_id_source,
+        _airbyte_raw_id,
+        case 
+            when position_name is null then null 
+            when regexp_replace(regexp_replace(trim(position_name), ' ', '-'), '[^a-zA-Z0-9-]', '') = '' then null 
+            when election_day is null then null 
+            when regexp_replace(regexp_replace(trim(election_day), ' ', '-'), '[^a-zA-Z0-9-]', '') = '' then null
+            else lower(concat_ws('__',
+                regexp_replace(regexp_replace(trim(position_name), ' ', '-'), '[^a-zA-Z0-9-]', ''),
+                regexp_replace(regexp_replace(trim(election_day), ' ', '-'), '[^a-zA-Z0-9-]', '')
+            )) 
+        end as br_contest_id
+from br_new
+-- what follows is the core substance of who is being selected for uploading to HubSpot
+where
+    -- separate the records with both phones and emails
+    phone <> ''
+    and email <> ''
+    -- remove any major party candidates
+    and not parties like '%Democrat%'
+    and not parties like '%Republican%'
+    -- subset to candidates with future elections
+    and election_day > current_date
+),
+br_withcodes AS (
+SELECT *,
+    {{ generate_candidate_code('first_name', 'last_name', 'state', 'office_type') }} AS br_candidate_code
+FROM br_formatted
+)
+SELECT * FROM br_withcodes
---
+++ dbt/project/models/intermediate/ballotready-to-hubspot/int__hubspot_ytd_candidacies.sql
@@ -0,0 +1,8 @@
+-- pulling in current year results 
+SELECT *
+FROM dbt.stg_airbyte_source__hubspot_api_contacts t1
+WHERE (
+    properties_type LIKE '%Self-Filer Lead%'
+    OR properties_product_user = 'yes'
+)
+AND properties_election_date BETWEEN DATE_TRUNC('year', CURRENT_DATE) AND DATE_TRUNC('year', CURRENT_DATE + INTERVAL 1 YEAR) - INTERVAL 1 DAY
