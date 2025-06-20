71e19a5a0eab95a7182c94aac334cb6c391f6d21
Hugh Karimi <hussain.h.karimi@gmail.com>
2025-06-19 22:13:33 -0400

    [DT-119] add hubspot engagements table (#63)

---
+++ dbt/project/models/staging/airbyte_source/hubspot_api/stg_airbyte_source__hubspot_api_engagements.sql
@@ -0,0 +1,4 @@
+{% set source_ref = source("airbyte_source", "hubspot_api_engagements") %}
+
+select {{ dbt_utils.star(from=source_ref, except=[]) }}  -- use `except` for any columns to transform individually
+from {{ source_ref }}
