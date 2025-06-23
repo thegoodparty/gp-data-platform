d3b931e0e9a00fcb9993f22644f534bedd106df7
Hugh Karimi <hussain.h.karimi@gmail.com>
2025-06-23 14:15:32 -0400

    add script to upload shape files

---
+++ scripts/load_us_census_shapefiles_to_databricks.py
@@ -0,0 +1,143 @@
+import os
+import zipfile
+from urllib.parse import urljoin
+
+import requests
+from bs4 import BeautifulSoup
+from databricks.sdk import WorkspaceClient
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=".env")
+
+DBX_SHAPEFILES_VOLUME_PATH = os.environ.get("DBX_SHAPEFILES_VOLUME_PATH")
+
+
+def download_files(url, save_dir):
+    """
+    Download multiple files from the internet and save them to a specified directory.
+
+    Parameters:
+    urls (list): List of file URLs to download.
+    save_dir (str): Directory to save the downloaded files.
+    """
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an error for failed requests
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching page: {e}")
+        return
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    zip_links = []
+
+    # Find all anchor tags
+    for link in soup.find_all("a", href=True):
+        href = link["href"]
+        if href.endswith(".zip"):
+            full_link = urljoin(url, href)  # Ensure absolute URL
+            zip_links.append(full_link)
+
+    # only keep WY for now
+    zip_links = [link for link in zip_links if "56" in link]
+
+    for url in zip_links:
+        try:
+            response = requests.get(url, stream=True)
+            response.raise_for_status()  # Raise an error for bad status codes
+
+            filename = os.path.join(save_dir, url.split("/")[-1])
+            with open(filename, "wb") as file:
+                for chunk in response.iter_content(chunk_size=8192):
+                    file.write(chunk)
+
+            print(f"Downloaded: {filename}")
+        except requests.RequestException as e:
+            print(f"Failed to download {url}: {e}")
+
+
+def unzip_all_files(directory):
+    """
+    Unzips all .zip files in the given directory.
+
+    Parameters:
+    directory (str): Path to the directory containing zip files.
+    """
+    if not os.path.exists(directory):
+        print(f"Directory '{directory}' does not exist.")
+        return
+
+    for filename in os.listdir(directory):
+        if filename.endswith(".zip"):
+            file_path = os.path.join(directory, filename)
+            unzipping_path = os.path.join(directory, filename.replace(".zip", ""))
+            if os.path.exists(unzipping_path):
+                print(f"Skipping {filename} because it already exists")
+                continue
+            try:
+                with zipfile.ZipFile(file_path, "r") as zip_ref:
+                    zip_ref.extractall(unzipping_path)
+                print(f"Unzipped: {file_path}")
+            except zipfile.BadZipFile:
+                print(f"Failed to unzip (corrupted file): {file_path}")
+            except Exception as e:
+                print(f"Error unzipping {file_path}: {e}")
+
+
+def upload_to_databricks(directory):
+    try:
+        # Use explicit authentication parameters
+        w = WorkspaceClient(
+            host=os.environ.get("DATABRICKS_HOST"),
+            token=os.environ.get("DATABRICKS_TOKEN"),
+        )
+    except ValueError as e:
+        print(f"Authentication error: {str(e)}")
+        return  # Exit the function if authentication fails
+
+    shapefile_directories = [
+        os.path.join(directory, d)
+        for d in os.listdir(directory)
+        if os.path.isdir(os.path.join(directory, d))
+    ]
+    for shapefile_directory in shapefile_directories:
+        volume_path = (
+            f"{DBX_SHAPEFILES_VOLUME_PATH}/{shapefile_directory.split('/')[-1]}"
+        )
+        w.dbfs.mkdirs(volume_path)
+        print(f"Uploading {shapefile_directory} to {volume_path}")
+        for file in os.listdir(shapefile_directory):
+            print(f"Uploading {file} to {volume_path}")
+            with open(f"{shapefile_directory}/{file}", "rb") as f:
+                w.files.upload(
+                    file_path=f"{volume_path}/{file}", contents=f, overwrite=True
+                )
+
+
+shapefiles_directory = "tmp_data/tiger_shapefiles"
+tiger_codes = [
+    "CD119",
+    "SLDU",
+    "SLDL",
+    "ELSD",
+    "SCSD",
+    "UNSD",
+    "SDADM",
+    "PLACE",
+    "COUSUB",
+    "COUNTY",
+    "CONCITY",
+    "STATE",
+    "COUNTY",
+]
+base_url = "https://www2.census.gov/geo/tiger/TIGER2024/"
+for tiger_code in tiger_codes:
+    if tiger_code == "CD119":
+        download_files(f"{base_url}/CD/", shapefiles_directory)
+    else:
+        download_files(f"{base_url}/{tiger_code}/", shapefiles_directory)
+
+unzip_all_files(shapefiles_directory)
+upload_to_databricks(shapefiles_directory)
