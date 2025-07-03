f0613a093a172afd67ae9c36257629f1d042bc3b
Tristan A <5641534+datawithtristan@users.noreply.github.com>
2025-07-03 14:46:19 -0700

    Ballotready to Hubspot model (#61)
    * Added files for obtaining BallotReady records populated with contact details that can be directly sent to HubSpot
    
    * Update dbt/project/models/intermediate/ballotready-to-hubspot/int__hubspot_ytd_candidacies.sql
    
    Co-authored-by: Hugh Karimi <hussain.h.karimi@gmail.com>
    
    * Update dbt/project/macros/variable-standardization/ballotready_standardizations.sql
    
    Co-authored-by: Hugh Karimi <hussain.h.karimi@gmail.com>
    
    * Update dbt/project/macros/variable-standardization/ballotready_standardizations.sql
    
    Co-authored-by: Hugh Karimi <hussain.h.karimi@gmail.com>
    
    * Update dbt/project/models/intermediate/ballotready-to-hubspot/int__ballotready_cleancandidacies.sql
    
    Co-authored-by: Hugh Karimi <hussain.h.karimi@gmail.com>
    
    * - Added tests for final file.
    - Added configs to each step.
    
    * running pre-commit
    
    * updated macro naming in clean_candidacies step
    
    ---------
    
    Co-authored-by: Tristan Aubert <tristan.aubert@goodparty.org>
    Co-authored-by: Hugh Karimi <hussain.h.karimi@gmail.com>

---
+++ dbt/project/models/intermediate/ballotready-to-hubspot/int__hubspot_candidacy_codes.sql
@@ -0,0 +1,18 @@
+{{ config(auto_liquid_cluster=true, tags=["intermediate", "hubspot"]) }}
+
+-- get the unique candidacies from the hubspot data
+select
+    {{
+        generate_candidate_code(
+            "properties_firstname",
+            "properties_lastname",
+            "properties_state",
+            "properties_office_type",
+        )
+    }} as hs_candidate_code
+from {{ ref("int__hubspot_ytd_candidacies") }}
+where
+    properties_firstname is not null
+    and properties_lastname is not null
+    and properties_state is not null
+    and properties_office_type is not null
