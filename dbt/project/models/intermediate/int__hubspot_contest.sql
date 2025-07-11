60383e0873fed31cb9aa3d1e10f611a37c9bffeb
Hugh Karimi <hussain.h.karimi@gmail.com>
2025-07-11 10:33:22 -0400

    fix intermediate columns

--- dbt/project/models/intermediate/int__hubspot_contest.sql
+++ dbt/project/models/intermediate/int__hubspot_contest.sql
@@ -26,25 +26,32 @@ select
     properties_candidate_office as candidate_office,
     properties_office_type as office_type,
     properties_office_level as office_level,
-    properties_partisanship_type as partisanship_type,
+    properties_partisan_type as partisan_type,
 
     -- geographic information
     properties_state as state,
     properties_city as city,
     properties_candidate_district as district,
-    case
-        when properties_official_office_name like '% - Seat %'
-        then regexp_extract(properties_official_office_name, ' - Seat ([^,]+)')
-        when properties_official_office_name like '% - Group %'
-        then regexp_extract(properties_official_office_name, ' - Group ([^,]+)')
-        when properties_official_office_name like '%, Seat %'
-        then regexp_extract(properties_official_office_name, ', Seat ([^,]+)')
-        when properties_official_office_name like '%: % - Seat %'
-        then regexp_extract(properties_official_office_name, ' - Seat ([^,]+)')
-        when properties_official_office_name like '% - Position %'
-        then regexp_extract(properties_official_office_name, ' - Position ([^\\s(]+)')
-        else ''
-    end as seat_name,
+    coalesce(
+        case
+            when properties_official_office_name like '% - Seat %'
+            then regexp_extract(properties_official_office_name, ' - Seat ([^,]+)')
+            when properties_official_office_name like '% - Group %'
+            then regexp_extract(properties_official_office_name, ' - Group ([^,]+)')
+            when properties_official_office_name like '%, Seat %'
+            then regexp_extract(properties_official_office_name, ', Seat ([^,]+)')
+            when properties_official_office_name like '%: % - Seat %'
+            then regexp_extract(properties_official_office_name, ' - Seat ([^,]+)')
+            when properties_official_office_name like '% - Position %'
+            then
+                regexp_extract(
+                    properties_official_office_name, ' - Position ([^\\s(]+)'
+                )
+            else null
+        end,
+        -- properties_candidate_seat,  # add in once updated in hubspot
+        ''
+    ) as seat_name,
 
     -- election context
     properties_number_opponents as number_of_opponents,
@@ -57,29 +64,31 @@ select
     properties_filing_deadline as filing_deadline,
     properties_primary_election_date as primary_election_date,
     properties_general_election_date as general_election_date,
-    properties_runoff_election_date as runoff_election_date,
     cast(null as date) as runoff_election_date,  -- get from campaign
 
     -- election results
     case
-        when today() < filing_deadline
+        when current_date() < filing_deadline
         then 'not started'
         when
-            today() >= filing_deadline
-            and today() <= max(general_election_date, runoff_election_date)
+            current_date() >= filing_deadline
+            and current_date() <= greatest(general_election_date, runoff_election_date)
         then 'in progress'
-        when today() > max(general_election_date, runoff_election_date)
+        when current_date() > greatest(general_election_date, runoff_election_date)
         then 'completed'
     end as contest_status,
-    null as primary_results,
-    null as general_election_results,
-    null as runoff_election_results,
-    null as contest_results
+    cast(null as string) as primary_results,
+    cast(null as string) as general_election_results,
+    cast(null as string) as runoff_election_results,
+    cast(null as string) as contest_results,
 
--- to add
--- term_length_years,
+    -- metadata
+    `updatedAt` as updated_at,
+    `createdAt` as created_at
+
+-- TODO: add term_length_years when available
 -- TODO: add tests
-from {{ ref("stg_airbyte_source__hubspot_api_companies") }}
+from {{ ref("stg_airbyte_source__hubspot_api_contacts") }}
 where
     1 = 1 and properties_official_office_name is not null
     {% if is_incremental() %}
