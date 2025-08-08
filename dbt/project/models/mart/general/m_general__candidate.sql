19816e8fce37fb54e75071d0a66b9c18ef4f3e2a
Hugh Karimi <hussain.h.karimi@gmail.com>
2025-08-08 17:16:01 -0400

    add more fields

--- dbt/project/models/mart/general/m_general__candidate.sql
+++ dbt/project/models/mart/general/m_general__candidate.sql
@@ -1,15 +1,53 @@
 {{
     config(
         materialized="incremental",
-        unique_key="id",
+        unique_key="hubspot_contact_id",
         on_schema_change="append_new_columns",
         auto_liquid_cluster=true,
         tags=["mart", "general", "candidacy", "hubspot"],
     )
 }}
 
-select tbl_hs_contacts.contact_id as hubspot_contact_id
+select
+    tbl_hs_contacts.contact_id as hubspot_contact_id,
+    tbl_gp_user.id as prod_db_user_id,
+    tbl_hs_contacts.candidate_id_source,
+    tbl_contest.gp_contest_id,
+
+    /*
+    -- Candidate information
+    -- Data source follows the following hierarchy:
+    -- gp db -> hs.companies -> hs.contact
+    */
+    coalesce(tbl_gp_user.name, tbl_hs_contacts.full_name) as candidate_name,
+    coalesce(tbl_gp_user.first_name, tbl_hs_contacts.first_name) as first_name,
+    coalesce(tbl_gp_user.last_name, tbl_hs_contacts.last_name) as last_name,
+    tbl_hs_contacts.birth_date,
+    coalesce(tbl_gp_user.email, tbl_hs_contacts.email) as candidate_email,
+    coalesce(tbl_gp_user.phone, tbl_hs_contacts.phone_number) as phone_number,
+
+    /*
+    -- Social media links
+    -- Hubspot > HS contact
+    */
+    tbl_hs_contacts.linkedin_url,
+    tbl_hs_contacts.instagram_handle,
+    tbl_hs_contacts.twitter_handle,
+    tbl_hs_contacts.facebook_url,
+    tbl_hs_contacts.website_url,
+    tbl_hs_contacts.street_address,
+
+    -- metadata timestamps
+    tbl_hs_contacts.created_at,
+    tbl_hs_contacts.updated_at
+
 from {{ ref("int__hubspot_contacts_w_companies") }} as tbl_hs_contacts
 left join
     {{ ref("stg_airbyte_source__gp_api_db_user") }} as tbl_gp_user
-    on tbl_hs_contacts.gp_candidacy_id = tbl_gp_user.data:candidacyid::string
+    on tbl_hs_contacts.contact_id = tbl_gp_user.meta_data:hubspotid::string
+left join
+    {{ ref("m_general__contest") }} as tbl_contest
+    on tbl_hs_contacts.contact_id = tbl_contest.contact_id
+{% if is_incremental() %}
+    where tbl_hs_contacts.updated_at >= (select max(updated_at) from {{ this }})
+{% endif %}
