10db50a1ccb0a9752dca871133ac5074f681a964
Hugh Karimi <hussain.h.karimi@gmail.com>
2025-08-20 12:49:51 -0400

    [DT-238] dbt: techspeed candidates to hubspot models

---
+++ dbt/project/models/intermediate/techspeed_to_hubspot/int__techspeed_candidates.sql
@@ -0,0 +1,132 @@
+{{ config(materialized="incremental", on_schema_change="append_new_columns") }}
+
+with
+    clean_candidates as (
+        select
+            candidate_id_source,
+            first_name,
+            last_name,
+            case
+                when upper(is_incumbent) = 'TRUE'
+                then 'Incumbent'
+                when upper(is_incumbent) = 'FALSE'
+                then 'Challenger'
+            end as candidate_type,
+            email,
+            replace(
+                replace(replace(replace(phone, '-', ''), '_', ''), '[', ''), ']', ''
+            ) as phone,
+            candidate_id_tier,
+            party,
+            website_url,
+            linkedin_url,
+            instagram_handle,
+            twitter_handle,
+            facebook_url,
+            date_of_birth_mmddyyyy as birth_date,
+            street_address,
+            postal_code,
+            district_name as district,
+            normalized_location as city,
+            state,
+            office_name as official_office_name,
+            office_normalized as candidate_office,
+            office_type,
+            office_level,
+            filing_deadline,
+            primary_election_date,
+            general_election_date,
+
+            -- Transform is_primary to Election Type
+            case
+                when
+                    (
+                        upper(is_primary) = 'YES'
+                        or upper(is_primary) = 'TRUE'
+                        or upper(is_primary) = 'PRIMARY'
+                    )
+                then 'Primary'
+                when
+                    (
+                        upper(is_primary) = 'NO'
+                        or upper(is_primary) = 'FALSE'
+                        or upper(is_primary) = 'GENERAL'
+                    )
+                then 'General'
+                else is_primary
+            end as election_type,
+
+            -- Transform is_uncontested to Uncontested
+            case
+                when upper(is_uncontested) = 'NO'
+                then 'Contested'
+                when upper(is_uncontested) = 'YES'
+                then 'Uncontested'
+                else is_uncontested
+            end as uncontested,
+            number_candidates as number_of_candidates,
+            seats_available as number_of_seats_available,
+            open_seat,
+            partisan,
+            population,
+            ballotready_race_id,
+
+            -- Assign constant values
+            'Self-Filer Lead' as `type`,
+            'jesse@goodparty.org' as contact_owner,
+            'Jesse Diliberto' as owner_name,
+
+            -- placeholder for "uploaded" column
+            case
+                when (phone is null and email is null) then 'no_contact' else null
+            end as uploaded,
+
+            _airbyte_extracted_at
+        from {{ ref("stg_airbyte_source__techspeed_gdrive_candidates") }}
+        {% if is_incremental() %}
+            where
+                _airbyte_extracted_at
+                > (select max(_airbyte_extracted_at) from {{ this }})
+        {% endif %}
+    )
+select
+    candidate_id_source,
+    first_name,
+    last_name,
+    candidate_type,
+    email,
+    phone,
+    candidate_id_tier,
+    party,
+    website_url,
+    linkedin_url,
+    instagram_handle,
+    twitter_handle,
+    facebook_url,
+    birth_date,
+    street_address,
+    postal_code,
+    district,
+    city,
+    state,
+    official_office_name,
+    candidate_office,
+    office_type,
+    office_level,
+    filing_deadline,
+    primary_election_date,
+    general_election_date,
+    election_type,
+    uncontested,
+    number_of_candidates,
+    number_of_seats_available,
+    open_seat,
+    partisan,
+    population,
+    ballotready_race_id,
+    type,
+    contact_owner,
+    owner_name,
+    uploaded,
+    _airbyte_extracted_at
+from clean_candidates
---
+++ dbt/project/models/intermediate/techspeed_to_hubspot/int__techspeed_candidates_clean_last_name.sql
@@ -0,0 +1,222 @@
+{{ config(materialized="incremental", on_schema_change="append_new_columns") }}
+
+with
+    candidates_wo_suffix as (
+        select
+            candidate_id_source,
+            first_name,
+            last_name,
+            candidate_type,
+            email,
+            phone,
+            candidate_id_tier,
+            party,
+            website_url,
+            linkedin_url,
+            instagram_handle,
+            twitter_handle,
+            facebook_url,
+            birth_date,
+            street_address,
+            postal_code,
+            district,
+            city,
+            state,
+            official_office_name,
+            candidate_office,
+            office_type,
+            office_level,
+            filing_deadline,
+            primary_election_date,
+            general_election_date,
+            election_type,
+            uncontested,
+            number_of_candidates,
+            number_of_seats_available,
+            open_seat,
+            partisan,
+            population,
+            ballotready_race_id,
+            type,
+            contact_owner,
+            owner_name,
+            uploaded,
+            _airbyte_extracted_at,
+            trim(
+                regexp_replace(
+                    regexp_replace(
+                        regexp_replace(
+                            regexp_replace(
+                                regexp_replace(
+                                    regexp_replace(
+                                        regexp_replace(
+                                            regexp_replace(
+                                                regexp_replace(
+                                                    regexp_replace(
+                                                        regexp_replace(
+                                                            regexp_replace(
+                                                                last_name, ' Jr$', ''
+                                                            ),
+                                                            ' Jr\.$',
+                                                            ''
+                                                        ),
+                                                        ' Sr$',
+                                                        ''
+                                                    ),
+                                                    ' Sr\.$',
+                                                    ''
+                                                ),
+                                                ' II$',
+                                                ''
+                                            ),
+                                            ' Ii$',
+                                            ''
+                                        ),
+                                        ' III$',
+                                        ''
+                                    ),
+                                    ' Iii$',
+                                    ''
+                                ),
+                                ' Iv$',
+                                ''
+                            ),
+                            ' I ii$',
+                            ''
+                        ),
+                        ' V$',
+                        ''
+                    ),
+                    ' Iiii$',
+                    ''
+                )
+            ) as last_name_no_suffix
+        from {{ ref("int__techspeed_candidates") }}
+        {% if is_incremental() %}
+            where
+                _airbyte_extracted_at
+                > (select max(_airbyte_extracted_at) from {{ this }})
+        {% endif %}
+    ),
+    candidates_w_clean_comma as (
+        select
+            * except (last_name_no_suffix),
+            -- Remove trailing commas
+            regexp_replace(last_name_no_suffix, ',$', '') as last_name_no_comma
+        from candidates_wo_suffix
+    ),
+    candidates_w_clean_space as (
+        select
+            * except (last_name_no_comma),
+            case
+                when
+                    length(last_name_no_comma) >= 2
+                    and substring(last_name_no_comma, length(last_name_no_comma) - 1, 1)
+                    = ' '
+                then substring(last_name_no_comma, 1, length(last_name_no_comma) - 2)
+                else last_name_no_comma
+            end as last_name_trimmed
+        from candidates_w_clean_comma
+    ),
+    candidates_w_clean_initials as (
+        select
+            * except (last_name_trimmed),
+            -- Handle middle initial with period but no space
+            case
+                when
+                    length(last_name_trimmed) >= 3
+                    and substring(last_name_trimmed, 2, 1) = '.'
+                    and substring(last_name_trimmed, 3, 1) != ' '
+                then substring(last_name_trimmed, 3)
+                else last_name_trimmed
+            end as last_name_clean
+        from candidates_w_clean_space
+    ),
+    candidates_w_extracted_last_name as (
+        select
+            * except (last_name_clean),
+            -- Extract suggested last name (everything after last space)
+            case
+                when last_name_clean like '% %'
+                then
+                    case
+                        -- Handle special prefixes that should be kept
+                        when last_name_clean like '%De La %'
+                        then
+                            'De La ' || substring(
+                                last_name_clean,
+                                position('De La ' in last_name_clean) + 7
+                            )
+                        -- Hugh: Why is the offset 7 here, instead of 5 (= len('de la
+                        -- '))?
+                        when last_name_clean like '%Van %'
+                        then
+                            'Van ' || substring(
+                                last_name_clean, position('Van ' in last_name_clean) + 4
+                            )
+                        when last_name_clean like '% van %'
+                        then
+                            ' van ' || substring(
+                                last_name_clean,
+                                position(' van ' in last_name_clean) + 5
+                            )
+                        when last_name_clean like '%Le %'
+                        then
+                            'Le ' || substring(
+                                last_name_clean, position('Le ' in last_name_clean) + 3
+                            )
+                        else
+                            -- Default: take everything after the last space
+                            substring(
+                                last_name_clean,
+                                length(last_name_clean)
+                                - position(' ' in reverse(last_name_clean))
+                                + 2
+                            )
+                    end
+                else last_name_clean
+            end as suggested_last
+        from candidates_w_clean_initials
+    )
+
+select
+    candidate_id_source,
+    first_name,
+    suggested_last as last_name,
+    candidate_type,
+    email,
+    phone,
+    candidate_id_tier,
+    party,
+    website_url,
+    linkedin_url,
+    instagram_handle,
+    twitter_handle,
+    facebook_url,
+    birth_date,
+    street_address,
+    postal_code,
+    district,
+    city,
+    state,
+    official_office_name,
+    candidate_office,
+    office_type,
+    office_level,
+    filing_deadline,
+    primary_election_date,
+    general_election_date,
+    election_type,
+    uncontested,
+    number_of_candidates,
+    number_of_seats_available,
+    open_seat,
+    partisan,
+    population,
+    ballotready_race_id,
+    type,
+    contact_owner,
+    owner_name,
+    uploaded,
+    _airbyte_extracted_at
+from candidates_w_extracted_last_name
