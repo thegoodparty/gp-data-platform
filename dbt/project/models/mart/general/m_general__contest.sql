0e857bf9b8e667a0225142fbc3643379577f5e64
Hugh Karimi <hussain.h.karimi@gmail.com>
2025-09-04 15:20:54 -0400

    [DT-255,DT-266] add DDHQ results to general candidacy and contest mart (#86)

--- dbt/project/models/mart/general/m_general__contest.sql
+++ dbt/project/models/mart/general/m_general__contest.sql
@@ -8,65 +8,88 @@
     )
 }}
 
-select
-    -- Identifiers
-    contact_id,
-    {{
-        generate_salted_uuid(
-            fields=[
-                "official_office_name",
-                "candidate_office",
-                "office_type",
-                "office_level",
-                "state",
-                "city",
-                "district",
-                "seat_name",
-            ]
-        )
-    }} as gp_contest_id,
+with
+    constests as (
+        select
+            -- Identifiers
+            tbl_contest.contact_id,
+            {{
+                generate_salted_uuid(
+                    fields=[
+                        "coalesce(tbl_contest.official_office_name, '')",
+                        "coalesce(tbl_contest.candidate_office, '')",
+                        "coalesce(tbl_contest.office_type, '')",
+                        "coalesce(tbl_contest.office_level, '')",
+                        "coalesce(tbl_contest.state, '')",
+                        "coalesce(tbl_contest.city, '')",
+                        "coalesce(tbl_contest.district, '')",
+                        "coalesce(tbl_contest.seat_name, '')",
+                    ]
+                )
+            }} as gp_contest_id,
 
-    -- office information
-    official_office_name,
-    candidate_office,
-    office_type,
-    office_level,
-    partisan_type as partisanship_type,
+            -- office information
+            tbl_contest.official_office_name,
+            tbl_contest.candidate_office,
+            tbl_contest.office_type,
+            tbl_contest.office_level,
+            tbl_contest.partisan_type as partisanship_type,
 
-    -- geographic information
-    state,
-    city,
-    district,
-    seat_name,
+            -- geographic information
+            tbl_contest.state,
+            tbl_contest.city,
+            tbl_contest.district,
+            tbl_contest.seat_name,
 
-    -- election context
-    number_of_opponents,
-    seats_available,
-    uncontested,
-    open_seat,
-    term_start_date,
-    -- term_length_years, -- to add
-    -- election dates
-    filing_deadline,
-    primary_election_date,
-    general_election_date,
-    runoff_election_date,
+            -- election context
+            tbl_contest.number_of_opponents,
+            tbl_contest.seats_available,
+            tbl_contest.uncontested,
+            tbl_contest.open_seat,
+            tbl_contest.term_start_date,
+            -- term_length_years, -- to add
+            -- election dates
+            tbl_contest.filing_deadline,
+            tbl_contest.primary_election_date,
+            tbl_contest.general_election_date,
+            tbl_contest.runoff_election_date,
 
-    -- election results
-    primary_results,
-    general_election_results,
-    runoff_election_results,
-    contest_status,
-    contest_results,
+            -- election results
+            tbl_contest.primary_results,
+            tbl_contest.general_election_results,
+            tbl_contest.runoff_election_results,
+            tbl_contest.contest_status,
+            tbl_contest.contest_results,
 
-    -- metadata
-    created_at,
-    updated_at
+            -- DDHQ matches
+            tbl_candidacy.gp_candidacy_id,
+            tbl_candidacy.ddhq_candidate,
+            tbl_candidacy.ddhq_race_name,
+            tbl_candidacy.ddhq_candidate_party,
+            tbl_candidacy.ddhq_is_winner,
+            tbl_candidacy.ddhq_race_id,
+            tbl_candidacy.ddhq_election_type,
+            tbl_candidacy.ddhq_date,
+            tbl_candidacy.ddhq_llm_confidence,
+            tbl_candidacy.ddhq_llm_reasoning,
+            tbl_candidacy.ddhq_top_10_candidates,
+            tbl_candidacy.ddhq_has_match,
 
-from {{ ref("int__hubspot_contest") }}
-where
-    1 = 1 and contest_status is not null
-    {% if is_incremental() %}
-        and updated_at >= (select max(updated_at) from {{ this }})
-    {% endif %}
+            -- metadata
+            tbl_contest.created_at,
+            tbl_contest.updated_at
+
+        from {{ ref("int__hubspot_contest") }} as tbl_contest
+        left join
+            {{ ref("m_general__candidacy") }} as tbl_candidacy
+            on tbl_contest.contact_id = tbl_candidacy.contact_id
+        where
+            tbl_contest.contest_status is not null
+            {% if is_incremental() %}
+                and tbl_contest.updated_at >= (select max(updated_at) from {{ this }})
+            {% endif %}
+    )
+
+select *
+from constests
 qualify row_number() over (partition by gp_contest_id order by updated_at desc) = 1
