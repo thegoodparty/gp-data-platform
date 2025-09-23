9c06b193f048f7d4bc3993e65690960bbaed5c5e
Hugh Karimi <hussain.h.karimi@gmail.com>
2025-09-23 16:54:44 -0400

    [DATA-1196] add Amplitude data to dbt

---
+++ dbt/project/models/staging/airbyte_source/amplitude_api/stg_airbyte_source__amplitude_api_events_list.sql
@@ -0,0 +1,29 @@
+with
+    source as (
+        select * from {{ source("airbyte_source", "amplitude_api_events_list") }}
+    ),
+    renamed as (
+        select
+            {{ adapter.quote("_airbyte_raw_id") }},
+            {{ adapter.quote("_airbyte_extracted_at") }},
+            {{ adapter.quote("_airbyte_meta") }},
+            {{ adapter.quote("_airbyte_generation_id") }},
+            {{ adapter.quote("id") }},
+            {{ adapter.quote("name") }},
+            {{ adapter.quote("value") }},
+            {{ adapter.quote("hidden") }},
+            {{ adapter.quote("totals") }},
+            {{ adapter.quote("deleted") }},
+            {{ adapter.quote("display") }},
+            {{ adapter.quote("autohidden") }},
+            {{ adapter.quote("non_active") }},
+            {{ adapter.quote("flow_hidden") }},
+            {{ adapter.quote("in_waitroom") }},
+            {{ adapter.quote("totals_delta") }},
+            {{ adapter.quote("clusters_hidden") }},
+            {{ adapter.quote("timeline_hidden") }}
+
+        from source
+    )
+select *
+from renamed
