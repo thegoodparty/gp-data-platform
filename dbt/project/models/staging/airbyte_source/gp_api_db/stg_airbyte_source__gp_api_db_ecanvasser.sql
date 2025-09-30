bd96e9524b54bfeb138eef7820c41d258ae38026
Hugh Karimi <hussain.h.karimi@gmail.com>
2025-09-30 17:10:11 -0400

    [DATA-1200] add voter outreach data

---
+++ dbt/project/models/staging/airbyte_source/gp_api_db/stg_airbyte_source__gp_api_db_ecanvasser.sql
@@ -0,0 +1,20 @@
+with
+    source as (select * from {{ source("airbyte_source", "gp_api_db_ecanvasser") }}),
+    renamed as (
+        select
+            {{ adapter.quote("_airbyte_raw_id") }},
+            {{ adapter.quote("_airbyte_extracted_at") }},
+            {{ adapter.quote("_airbyte_meta") }},
+            {{ adapter.quote("_airbyte_generation_id") }},
+            {{ adapter.quote("id") }},
+            {{ adapter.quote("error") }},
+            {{ adapter.quote("api_key") }},
+            {{ adapter.quote("last_sync") }},
+            {{ adapter.quote("created_at") }},
+            {{ adapter.quote("updated_at") }},
+            {{ adapter.quote("campaign_id") }}
+
+        from source
+    )
+select *
+from renamed
