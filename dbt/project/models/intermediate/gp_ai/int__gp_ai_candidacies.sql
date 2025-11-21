d932aa869822f20cd6fc5edc4bf557d495b8797b
Hugh Karimi <hussain.h.karimi@gmail.com>
2025-11-21 14:25:14 -0500

    [DATA-1336] use general mart v2 to supply AI election matching job

---
+++ dbt/project/models/intermediate/gp_ai/int__gp_ai_candidacies.sql
@@ -0,0 +1,39 @@
+-- create a data table that joins m_general_candidacy_v2 with m_general_candidate_v2
+-- as the source
+-- table for the int__gp_ai_start_election_match job
+{{
+    config(
+        materialized="incremental",
+        unique_key="gp_candidacy_id",
+        on_schema_change="append_new_columns",
+        auto_liquid_cluster=true,
+        tags=["intermediate", "gp_ai", "candidacies"],
+    )
+}}
+
+with
+    candidacies as (
+        select *
+        from {{ ref("m_general__candidacy_v2") }}
+        {% if is_incremental() %}
+            where updated_at > (select max(updated_at) from {{ this }})
+        {% endif %}
+    ),
+    candidates as (select * from {{ ref("m_general__candidate_v2") }})
+
+select
+    candidacies.gp_candidacy_id,
+    candidates.first_name,
+    candidates.last_name,
+    candidates.state,
+    candidacies.candidate_office,
+    candidacies.official_office_name,
+    candidacies.office_level,
+    candidacies.party_affiliation,
+    candidacies.primary_election_date,
+    candidacies.general_election_date,
+    candidacies.runoff_election_date,
+    candidacies.created_at,
+    candidacies.updated_at
+from candidacies
+left join candidates on candidacies.gp_candidate_id = candidates.gp_candidate_id
--- dbt/project/models/intermediate/gp_ai/int__gp_ai_start_election_match.py
+++ dbt/project/models/intermediate/gp_ai/int__gp_ai_start_election_match.py
@@ -167,10 +167,14 @@ def model(dbt, session: SparkSession) -> DataFrame:
     gp_ai_url = dbt.config.get("gp_ai_url")
 
     # make the following references to maintain lineage
-    candidacy_table_name = "m_general__candidacy"
+    # candidacy_table_name = "m_general__candidacy"
+    candidacy_table_name = "int__gp_ai_candidacies"
     election_results_table_name = "stg_airbyte_source__ddhq_gdrive_election_results"
+    # candidacy_table: DataFrame = dbt.ref(
+    #     "m_general__candidacy"
+    # )  # dbt ref only supports string literals
     candidacy_table: DataFrame = dbt.ref(
-        "m_general__candidacy"
+        "int__gp_ai_candidacies"
     )  # dbt ref only supports string literals
     election_results_table: DataFrame = dbt.ref(
         "stg_airbyte_source__ddhq_gdrive_election_results"
@@ -198,7 +202,7 @@ def model(dbt, session: SparkSession) -> DataFrame:
             response_data = _create_election_match_request(
                 gp_ai_url=gp_ai_url,
                 gp_ai_api_key=gp_ai_api_key,
-                schema="dbt",
+                schema="dbt_hugh",
                 candidacy_table_name=candidacy_table_name,
                 election_results_table_name=election_results_table_name,
             )
@@ -214,7 +218,7 @@ def model(dbt, session: SparkSession) -> DataFrame:
     response_data = _create_election_match_request(
         gp_ai_url=gp_ai_url,
         gp_ai_api_key=gp_ai_api_key,
-        schema="dbt",
+        schema="dbt_hugh",
         candidacy_table_name=candidacy_table_name,
         election_results_table_name=election_results_table_name,
     )
