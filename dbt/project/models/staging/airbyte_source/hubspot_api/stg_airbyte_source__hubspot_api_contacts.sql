0eefe8e6bf598e25d1c9b8df871849028b34132b
Hugh Karimi <hussain.h.karimi@gmail.com>
2025-12-23 22:44:31 -0500

    undo array format change

--- dbt/project/models/staging/airbyte_source/hubspot_api/stg_airbyte_source__hubspot_api_contacts.sql
+++ dbt/project/models/staging/airbyte_source/hubspot_api/stg_airbyte_source__hubspot_api_contacts.sql
@@ -1,14 +1,6 @@
 select
     -- fmt: off
-    * except (companies, createdAt, updatedAt),
-    case
-        when
-            companies is null
-            or trim(companies) = ''
-            or trim(companies) = '[]'
-        then null
-        else from_json(companies, 'array<string>')
-    end as companies,  -- type is array<string>
+    * except (createdAt, updatedAt),
     createdAt as created_at,
     updatedAt as updated_at
     -- fmt: on
