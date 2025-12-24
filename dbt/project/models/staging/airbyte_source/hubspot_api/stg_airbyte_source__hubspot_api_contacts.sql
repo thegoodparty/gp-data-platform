d31b84ad5fbbb630e1b40afdd5c02f4a1406bc13
Hugh Karimi <hussain.h.karimi@gmail.com>
2025-12-23 22:23:36 -0500

    hotfix: update tcr stg table schema

--- dbt/project/models/staging/airbyte_source/hubspot_api/stg_airbyte_source__hubspot_api_contacts.sql
+++ dbt/project/models/staging/airbyte_source/hubspot_api/stg_airbyte_source__hubspot_api_contacts.sql
@@ -1,6 +1,14 @@
 select
     -- fmt: off
-    * except (createdAt, updatedAt),
+    * except (companies, createdAt, updatedAt),
+    case
+        when
+            companies is null
+            or trim(companies) = ''
+            or trim(companies) = '[]'
+        then null
+        else from_json(companies, 'array<string>')
+    end as companies,  -- type is array<string>
     createdAt as created_at,
     updatedAt as updated_at
     -- fmt: on
