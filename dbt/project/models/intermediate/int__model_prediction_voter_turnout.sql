7a5818183985bc44acf2f05f4f556817326a695b
Hugh Karimi <hussain.h.karimi@gmail.com>
2026-02-25 18:56:03 -0500

    Add turnout projection models for 2026 v2, 2027, and 2028 (#210)

--- dbt/project/models/intermediate/int__model_prediction_voter_turnout.sql
+++ dbt/project/models/intermediate/int__model_prediction_voter_turnout.sql
@@ -27,12 +27,13 @@ with
             model_version,
             inference_at
         from {{ ref("stg_model_predictions__turnout_projections_model2odd") }}
-        {% if is_incremental() %}
-            where
-                inference_at >= coalesce(
+        where
+            election_year not in (2027)
+            {% if is_incremental() %}
+                and inference_at >= coalesce(
                     (select max(inference_at) from {{ this }}), '1900-01-01'::timestamp
                 )
-        {% endif %}
+            {% endif %}
         qualify
             row_number() over (
                 partition by
@@ -46,7 +47,7 @@ with
             )
             = 1
     ),
-    even_year_projections as (
+    even_year_projections_pre_2026 as (
         select
             ballots_projected,
             state,
@@ -57,12 +58,106 @@ with
             model_version,
             inference_at
         from {{ ref("stg_model_predictions__turnout_projections_even_years_20250709") }}
-        {% if is_incremental() %}
-            where
-                inference_at >= coalesce(
+        where
+            election_year < 2026
+            {% if is_incremental() %}
+                and inference_at >= coalesce(
                     (select max(inference_at) from {{ this }}), '1900-01-01'::timestamp
                 )
-        {% endif %}
+            {% endif %}
+        qualify
+            row_number() over (
+                partition by
+                    state,
+                    district_type,
+                    district_name,
+                    election_year,
+                    election_code,
+                    model_version
+                order by inference_at desc
+            )
+            = 1
+    ),
+    projections_2026_v2 as (
+        select
+            ballots_projected,
+            state,
+            district_type,
+            district_name,
+            election_year,
+            election_code,
+            model_version,
+            inference_at
+        from {{ ref("stg_model_predictions__ballots_projected_2026_v2") }}
+        where
+            election_year = 2026
+            {% if is_incremental() %}
+                and inference_at >= coalesce(
+                    (select max(inference_at) from {{ this }}), '1900-01-01'::timestamp
+                )
+            {% endif %}
+        qualify
+            row_number() over (
+                partition by
+                    state,
+                    district_type,
+                    district_name,
+                    election_year,
+                    election_code,
+                    model_version
+                order by inference_at desc
+            )
+            = 1
+    ),
+    projections_2027 as (
+        select
+            ballots_projected,
+            state,
+            district_type,
+            district_name,
+            election_year,
+            election_code,
+            model_version,
+            inference_at
+        from {{ ref("stg_model_predictions__ballots_projected_2027") }}
+        where
+            election_year = 2027
+            {% if is_incremental() %}
+                and inference_at >= coalesce(
+                    (select max(inference_at) from {{ this }}), '1900-01-01'::timestamp
+                )
+            {% endif %}
+        qualify
+            row_number() over (
+                partition by
+                    state,
+                    district_type,
+                    district_name,
+                    election_year,
+                    election_code,
+                    model_version
+                order by inference_at desc
+            )
+            = 1
+    ),
+    projections_2028 as (
+        select
+            ballots_projected,
+            state,
+            district_type,
+            district_name,
+            election_year,
+            election_code,
+            model_version,
+            inference_at
+        from {{ ref("stg_model_predictions__ballots_projected_2028") }}
+        where
+            election_year = 2028
+            {% if is_incremental() %}
+                and inference_at >= coalesce(
+                    (select max(inference_at) from {{ this }}), '1900-01-01'::timestamp
+                )
+            {% endif %}
         qualify
             row_number() over (
                 partition by
@@ -81,4 +176,13 @@ select *
 from odd_year_projections
 union all
 select *
-from even_year_projections
+from even_year_projections_pre_2026
+union all
+select *
+from projections_2026_v2
+union all
+select *
+from projections_2027
+union all
+select *
+from projections_2028
