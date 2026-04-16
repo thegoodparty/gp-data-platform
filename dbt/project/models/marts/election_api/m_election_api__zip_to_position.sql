d931f099edeae03c2f0eed53ab9de02007111324
Hugh Karimi <hussain.h.karimi@gmail.com>
2026-04-16 15:28:53 -0400

    [DATA-1865] Add OfficePicker zip-to-position lookup model (#310)

---
+++ dbt/project/models/marts/election_api/m_election_api__zip_to_position.sql
@@ -0,0 +1,59 @@
+{{
+    config(
+        materialized="table",
+        tags=["mart", "election_api", "officepicker"],
+    )
+}}
+
+with
+    future_elections as (
+        select
+            election_date,
+            election_year,
+            state,
+            office_level,
+            office_type,
+            city,
+            district,
+            is_judicial,
+            br_position_database_id
+        from {{ ref("election") }}
+        where
+            election_date > current_date()
+            and election_date <= current_date() + interval 2 years
+    ),
+
+    zip_to_position as (
+        select zip_code, br_database_id from {{ ref("int__zip_code_to_br_office") }}
+    ),
+
+    positions as (
+        select id as position_id, name, br_database_id
+        from {{ ref("m_election_api__position") }}
+        where district_id is not null
+    ),
+
+    officepicker as (
+        select
+            pos.position_id,
+            pos.name,
+            zips.zip_code,
+            elec.election_year,
+            case
+                when elec.is_judicial then 'Judicial' else elec.office_level
+            end as display_office_level,
+            elec.office_type,
+            elec.state,
+            elec.city,
+            elec.district,
+            elec.election_date,
+            elec.br_position_database_id as br_database_id
+        from future_elections as elec
+        left join
+            zip_to_position as zips
+            on zips.br_database_id = elec.br_position_database_id
+        inner join positions as pos on pos.br_database_id = elec.br_position_database_id
+    )
+
+select *
+from officepicker
